9f5775701441d9ade6f318fd2def7f276e5c2cf6
StevenAston <steven.james.aston@gmail.com>
2014-06-16 15:35:05 -0400

    Minor formatting changes

--- Euler010b/src/Euler010b.cpp
+++ Euler010b/src/Euler010b.cpp
@@ -4,9 +4,9 @@
 // Version     :
 // Copyright   : 
 // Description : The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
-//				 Find the sum of all the primes below two million.
+//				 Find the product of all the primes below two million.
 //
-//				 Answer: 142913828922
+//				 Answer is about 849,000 digits
 //
 //============================================================================
 
